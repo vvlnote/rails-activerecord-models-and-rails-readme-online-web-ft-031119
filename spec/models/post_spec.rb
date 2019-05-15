a2b7db98413884417e07aa350177ae7a9ae43923
Joy Lin <vvlnote@hotmail.com>
2019-05-15 20:33:03 +0000

    Automatically backed up by Learn

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -7,7 +7,7 @@ describe Post do
   end
   
   it 'has a summary' do
-    post = Post.create!(title: "My title", description: "The post description")
+    post =Post.create!(title: "My title", description: "The post description")
     expect(post.post_summary).to eq("My title - The post description")
   end
 end
